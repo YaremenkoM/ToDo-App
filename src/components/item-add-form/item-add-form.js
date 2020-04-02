b75b73b83f13870bf2db420babb2986d09254005
YaremenkoM <mariayaremenko13@gmail.com>
2020-04-02 23:09:51 +0300

    adding todo items ability done

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './item-add-form.css';
 
-const ItemAddForm = ({addItem}) => {
-    return(
-        <form className="bottom-panel d-flex">
+export default class ItemAddForm extends Component {
+
+    state = {
+      label: ''
+    }
+
+    onLabelChange = (e) => {
+      this.setState({
+        label: e.target.value
+      })
+    }
+
+    onSubmit = (e) => {
+      e.preventDefault()
+      this.props.addItem(this.state.label)
+    }
+
+    render() {
+
+      const {addItem, label} = this.props;
+
+      return(
+        <form className="bottom-panel d-flex"
+              onSubmit={this.onSubmit}>
 
         <input type="text"
                className="form-control new-todo-label"
-               placeholder="What needs to be done?" />
+               placeholder="What needs to be done?" 
+               onChange={this.onLabelChange}
+               />
 
         <button type="submit"
-                className="btn btn-outline-secondary"
-                onClick={() => addItem('Be ind')}
-                >
+                className="btn btn-outline-secondary">
                     Add
         </button>
       </form>
     )
+  }
 }
-
-export default ItemAddForm;
